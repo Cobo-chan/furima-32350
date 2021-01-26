76a4d7fe53dbde800c0f7cfdcde42ddf87a32d6a
Cobo-chan <ota2dbxc5up@gmail.com>
2021-01-26 14:25:54 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do |v|
+    v.validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday
+  end
 end
