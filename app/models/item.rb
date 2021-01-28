279f946ff811ff10edaec7fe3bd33ebc48be75d5
Cobo-chan <ota2dbxc5up@gmail.com>
2021-01-28 17:46:50 +0900

    ActiveHashのモデル作成とアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtentions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :derivery_date
+  belongs_to :prefecture
+  belongs_to :selling_price
+  belongs_to :shipment_charge
 end
