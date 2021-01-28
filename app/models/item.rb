19eabda63d4fbcb060e66174523187bdc4691090
Cobo-chan <ota2dbxc5up@gmail.com>
2021-01-28 19:03:38 +0900

    ActiveHash修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtentions
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :derivery_date
