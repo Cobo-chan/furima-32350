a4faae1b57ffbf346460c9988eba850fba2b41bb
Cobo-chan <ota2dbxc5up@gmail.com>
2021-01-31 12:37:24 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
 
   with_options presence: true do |v|
-    v.validates :name, :text, :category_id, :condition_id, :shipment_charge_id, :prefecture_id, :derivery_date_id, :selling_price
+    v.validates :image, :name, :text, :category_id, :condition_id, :shipment_charge_id, :prefecture_id, :derivery_date_id, :selling_price
   end
   with_options numericality: { other_than: 1 } do
     validates :category_id, :condition_id, :shipment_charge_id, :prefecture_id, :derivery_date_id
   end
   validates :selling_price, numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999 }
-
+ 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
