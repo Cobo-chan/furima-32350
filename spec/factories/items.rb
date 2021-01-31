f3d557baeb1eca7a999b4e85076c2fcc6057c328
Cobo-chan <ota2dbxc5up@gmail.com>
2021-01-31 12:37:55 +0900

    itemモデルのテストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                { Faker::Lorem.sentence }
+    text                { Faker::Lorem.sentence }
+    category_id         { Faker::Number.within(range: 2..10) }
+    condition_id        { Faker::Number.within(range: 2..8) }
+    derivery_date_id    { Faker::Number.within(range: 2..4) }
+    shipment_charge_id  { Faker::Number.within(range: 2..3) }
+    prefecture_id       { Faker::Number.within(range: 2..48) }
+    selling_price    { Faker::Number.number(digits: 6) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/dl-android.png'), filename: 'dl-android.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '出品機能' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    context '商品が出品できる場合' do
+      it '画像、商品名、説明文、カテゴリー、状態、配送料負担、発送元、発送までの日数、販売金額が存在すれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品が出品できない場合' do
+      it '画像が添付されていないと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名がないと出品できない'do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品説明がないと出品できない' do
+        @item.text = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'カテゴリーが未選択だと出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'コンディションが未選択だと出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+      end
+      it '配送料負担が未選択だと出品できない' do
+        @item.shipment_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipment charge must be other than 1")
+      end
+      it '発送元が未選択だと出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it '発送までの日数が未選択だと出品できない' do
+        @item.derivery_date_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Derivery date must be other than 1")
+      end
+      it '販売価格が空だと出品できない' do
+        @item.selling_price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Selling price can't be blank")
+      end
+      it '販売価格が300円以下だと出品できない' do
+        @item.selling_price = 200
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Selling price must be greater than 300")
+      end
+      it '販売価格が9,999,999円以上だと出品できない' do
+        @item.selling_price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Selling price must be less than 9999999")
+      end
+      it '販売価格が全角入力だと出品できない' do
+        #全角入力は文字列として返されるため文字列を代入してテストを実行した
+        @item.selling_price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Selling price is not a number")
+      end
+    end
+  
+  end
 end
