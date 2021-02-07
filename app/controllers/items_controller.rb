652771760c6f698fbb2ccc3a8dec8bd2d9e711a2
Cobo-chan <ota2dbxc5up@gmail.com>
2021-02-07 13:52:42 +0900

    編集機能とエラーハンドリングの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new]
-
+  before_action :move_to_index, only:[:edit]
   def index
     @item = Item.all.order('created_at DESC').includes(:user)
   end
@@ -26,8 +26,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(create_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
   def create_params
     params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :shipment_charge_id, :prefecture_id, :derivery_date_id, :selling_price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    if user_signed_in?
+      item = Item.find(params[:id])
+      unless current_user.id == item.user.id
+        redirect_to root_path
+      end
+    else
+      redirect_to root_path
+    end
   end
 end
