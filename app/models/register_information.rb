86b368e884fe27f7f64c6a99e9abb62bef809c20
Cobo-chan <ota2dbxc5up@gmail.com>
2021-02-14 18:12:13 +0900

    エラーメッセージの修正

--- app/models/register_information.rb
+++ app/models/register_information.rb
@@ -4,9 +4,9 @@ class RegisterInformation
   with_options presence: true do
     validates :postcode, :municipality, :address, :phone_number, :token
   end
-  validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Postal code Input currectly"}
+  validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Input currectly"}
   validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
-  validates :phone_number, numericality: { integer: true, with: /\A[a-zA-Z0-9]+\z/, message: "Phone number Input only number" }
+  validates :phone_number, numericality: { integer: true, with: /\A[a-zA-Z0-9]+\z/, message: "Input only number" }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
