7021db63660a164894c3e646000c23854a45a8e5
Cobo-chan <ota2dbxc5up@gmail.com>
2021-02-19 11:14:22 +0900

    バリデーションとアソシエーションの修正

--- app/models/register_information.rb
+++ app/models/register_information.rb
@@ -6,7 +6,8 @@ class RegisterInformation
   end
   validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Input currectly"}
   validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
-  validates :phone_number, numericality: { integer: true, with: /\A\d{11}[0-9]\z/, message: "Input only number" }
+  validates :phone_number, numericality: { integer: true }
+  validates :phone_number, format: { with: /\A\d{11}\z/, message: "Input 11characters" }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
