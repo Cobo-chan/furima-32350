130e6ba0b70a5fcbdc7977f949521f5b1eb14351
Cobo-chan <ota2dbxc5up@gmail.com>
2021-02-19 11:14:41 +0900

    テストコードの実装

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アソシエーションの確認' do
+    before do
+      @order = FactoryBot.create(:order)
+    end
+
+    it 'orderに紐づくuserがなければ保存できない' do
+      @order.user = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("User must exist")
+    end
+    it 'orderに紐づくitemがなければ保存できない' do
+      @order.item = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Item must exist")
+    end
+  end
 end
